c0d4336d7a87882e51d72c923843cb9c16dd5455
Phucdh141@gmail.com <Phucdh141@gmail.com>
2025-10-24 14:35:22 +0700

    Assignment

--- Database/Bài tập/Assignment4.sql
+++ Database/Bài tập/Assignment4.sql
@@ -29,26 +29,67 @@ SELECT CategoryID, (
 FROM Question
 GROUP BY CategoryID;
 -- Question 7: Thông kê mỗi Question được sử dụng trong bao nhiêu Exam
-
+SELECT QuestionID, Content, (
+		SELECT COUNT(*) FROM ExamQuestion WHERE ExamQuestion.QuestionID = Question.QuestionID
+) AS Total    
+FROM Question;
 -- Question 8: Lấy ra Question có nhiều câu trả lời nhất
-
+SELECT QuestionID, Content, (
+	SELECT COUNT(*) FROM Answer	WHERE Answer.QuestionID = Question.QuestionID
+) AS TotalAnswer    
+FROM Question
+ORDER BY TotalAnswer DESC
+LIMIT 1;
 -- Question 9: Thống kê số lượng account trong mỗi group
-
+SELECT GroupID, COUNT(AccountID) AS TotalAccount
+FROM GroupAccount
+GROUP BY GroupID;
 -- Question 10: Tìm chức vụ có ít người nhất
-
+SELECT PositionID, PositionName,(
+		SELECT COUNT(*) FROM `Account` a WHERE a.PositionID =  p.PositionID
+) AS Total
+FROM `Position` p
+WHERE (SELECT COUNT(*) FROM `Account` a WHERE a.PositionID = p.PositionID) = (
+    SELECT MIN(SoLuong) 
+    FROM (
+        SELECT COUNT(*) AS SoLuong
+        FROM `Account` 
+        GROUP BY PositionID
+    ) AS Temp
+);
 -- Question 11: Thống kê mỗi phòng ban có bao nhiêu dev, test, scrum master, PM
+SELECT 
+  d.DepartmentID,
+  d.DepartmentName,
+  (SELECT COUNT(*) FROM `Account` a 
+     WHERE a.DepartmentID = d.DepartmentID AND a.PositionID = 1) AS Dev,
+  (SELECT COUNT(*) FROM `Account` a 
+     WHERE a.DepartmentID = d.DepartmentID AND a.PositionID = 2) AS Test,
+  (SELECT COUNT(*) FROM `Account` a 
+     WHERE a.DepartmentID = d.DepartmentID AND a.PositionID = 3) AS ScrumMaster,
+  (SELECT COUNT(*) FROM `Account` a 
+     WHERE a.DepartmentID = d.DepartmentID AND a.PositionID = 4) AS PM
+FROM Department d;
 
 -- Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, …
 
-
 -- Question 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
 
--- Question 14:Lấy ra group không có account nào
-
+-- Question 14: Lấy ra group không có account nào
 -- Question 15: Lấy ra group không có account nào
-
+SELECT *
+FROM `Group`
+WHERE GroupID NOT IN (
+    SELECT GroupID
+    FROM GroupAccount
+);
 -- Question 16: Lấy ra question không có answer nào
-
+SELECT *
+FROM Question
+WHERE QuestionID NOT IN (
+    SELECT QuestionID
+    FROM Answer
+);
 /*
 Question 17:
 a) Lấy các account thuộc nhóm thứ 1
